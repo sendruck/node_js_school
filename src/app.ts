e746342ced1c4a638469e7e413c10c517645df2d
Oleksandr Yakovenko <oleksandr.yakovenko@dataart.com>
2022-01-22 00:26:57 +0200

    Added fixes to start app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
+import 'reflect-metadata';
+
 import express from 'express';
 import config from './config';
 import Logger from './loaders/logger';
 
 async function startServer() {
 	const app = express();
+
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	await require('./loaders').default({ expressApp: app });
 
 	app.listen(config.port, () => {
 		Logger.info(`Express loaded: ${config.port}`);
--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,7 @@ if (envFound.error) {
 }
 
 export default {
-	port: parseInt(process.env.PORT, 10),
+	port: parseInt(process.env.APP_PORT, 10),
 	logs: {
 		level: process.env.LOG_LEVEL || 'silly',
 	},
